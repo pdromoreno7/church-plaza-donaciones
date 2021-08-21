df2602ba2c95778e0c1a319a47e87dc6e08db511
pdromoreno7 <pdro.moreno96@gmail.com>
2021-08-21 13:51:02 -0500

    Navbar estructura jsx terminada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import styled from "styled-components";
+import { CgMenu } from "react-icons/cg";
+
+const NavMenuStyles = styled.nav`
+  color: var(--gray-1);
+`;
 
 export default function Navbar() {
   return (
-    <div>
-      <h1>Hola sou Nav</h1>
-    </div>
+    <NavMenuStyles>
+      <div className="wrapper">
+        <div className="nav__logo">Logo</div>
+
+        <div className="nav__icon-menu">
+          <CgMenu />
+        </div>
+
+        <ul className="nav__menu">
+          <li className="nav__item">
+            <a href="#home" className="nav__link">
+              Inicio
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#proyect" className="nav__link">
+              Proyecto
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#donaciones" className="nav__link">
+              Donaciones
+            </a>
+          </li>
+        </ul>
+      </div>
+    </NavMenuStyles>
   );
 }
