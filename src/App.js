b89644777e4d8fdd0298adffe260bfb093fc6f38
pdromoreno7 <pdro.moreno96@gmail.com>
2021-08-25 23:03:53 -0500

    Añadimos acrivo proyectSection.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Hero } from "./components/Hero";
 import Navbar from "./components/Navbar";
+import { ProyectSection } from "./components/ProyectSection";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <ProyectSection />
     </>
   );
 }
---
+++ src/components/ProyectSection.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function ProyectSection() {
+  return (
+    <div>
+      <h1>Proyect</h1>
+    </div>
+  );
+}
