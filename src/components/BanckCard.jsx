0ab73ba40c65d638a6792973c8042f16eac59d09
pdromoreno7 <pdro.moreno96@gmail.com>
2021-08-26 23:18:21 -0500

    Se termino estilos base en BanckCard

--- src/components/BanckCard.jsx
+++ src/components/BanckCard.jsx
@@ -1,5 +1,43 @@
 import React from "react";
+import styled from "styled-components";
+import { Button } from "./Button";
 
-export function BanckCard() {
-  return <div></div>;
+const BanckStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  width: 566px;
+  height: 283px;
+  padding: 30px 25px;
+  border-radius: 5px;
+  background: var(--card-gradient-color);
+  color: var(--white);
+  .card__info p {
+    padding-bottom: 1rem;
+  }
+  .card__info-number {
+    letter-spacing: 0.2rem;
+  }
+  .card__button {
+    align-self: flex-end;
+    color: black;
+    /* justify-self: end; */
+  }
+`;
+
+export function BanckCard({ logoBanck, number }) {
+  return (
+    <BanckStyle>
+      <figure>
+        <img src={logoBanck} alt="Icono de banco" />
+      </figure>
+      <div className="card__info">
+        <p>Numero de cuenta</p>
+        <p className="card__info-number">{number}</p>
+      </div>
+      <div className="card__button">
+        <Button btnText="Donar" />
+      </div>
+    </BanckStyle>
+  );
 }
--- src/components/Donaciones.jsx
+++ src/components/Donaciones.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { BanckCard } from "./BanckCard";
+import NequiLogo from "../assets/nequi-logo.svg";
 
 export function Donaciones() {
   return (
     <div>
-      <h1>Donaciones</h1>
+      <div className="bd-container">
+        <h1>Donaciones</h1>
+        <BanckCard logoBanck={NequiLogo} number={3144516187} />
+      </div>
     </div>
   );
 }
