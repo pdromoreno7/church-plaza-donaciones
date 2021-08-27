a0bfdef2e830ada7d09df731936b367308df4bf6
pdromoreno7 <pdro.moreno96@gmail.com>
2021-08-27 11:36:57 -0500

    Se termino estilos en el footer mara mobile y escritotio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,89 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from "react";
 import styled from "styled-components";
+import footerLogo from "../assets/LogoGrey-footer.svg";
+import { TiSocialYoutube } from "react-icons/ti";
 
 const FooterStyles = styled.footer`
   width: 100%;
   border-top: 1px solid #979797;
+  .footer__container {
+    margin-top: 50px;
+    margin-bottom: 65px;
+    display: flex;
+    gap: 3rem;
+  }
+  .footer__nav,
+  .footer__contact {
+    display: flex;
+    flex-direction: column;
+    gap: 1.3rem;
+
+    ul {
+      line-height: 2rem;
+      li:hover {
+        color: var(--font-color-2);
+        transition: 0.2s ease-out;
+      }
+    }
+  }
+  .footer__nav-title {
+    color: var(--font-color-2);
+    font-weight: var(--medium-font);
+  }
+
+  .footer__contact a {
+    color: var(--font-color);
+    display: flex;
+    align-items: center;
+    gap: 0.3rem;
+    svg {
+      font-size: 1.2rem;
+    }
+    :hover {
+      color: var(--font-color-2);
+      transition: 0.2s ease-out;
+    }
+  }
+  @media screen and (max-width: 960px) {
+    .footer__container {
+      justify-content: center;
+    }
+  }
+  @media screen and (max-width: 700px) {
+    .footer__container {
+      flex-direction: column;
+      align-items: center;
+      text-align: center;
+    }
+  }
 `;
 
 export function Footer() {
   return (
     <FooterStyles>
-      <div className="bd-container">
-        <h1>Footer</h1>
+      <div className="footer__container bd-container">
+        <div className="footer__logo">
+          <img src={footerLogo} alt="Logo fuente de poder " />
+        </div>
+        <div className="footer__nav">
+          <p className="footer__nav-title">Funciones</p>
+          <ul>
+            <li>Inicio</li>
+            <li>Nombre del proyecto</li>
+            <li>Medios de donación</li>
+          </ul>
+        </div>
+        <div className="footer__contact">
+          <p className="footer__nav-title">Miranos en</p>
+          <a
+            href="https://www.youtube.com/channel/UCUKthUop2EVI6neyzsgiA8A"
+            target="_blank"
+          >
+            <TiSocialYoutube />
+            Iglesia Fuente de Poder
+          </a>
+        </div>
       </div>
     </FooterStyles>
   );
